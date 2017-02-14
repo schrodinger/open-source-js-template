4a27f4b04a9e02eae26cc5079bcc563eb678af1b
Paul Novak <paul.novak@schrodinger.com>
2017-02-14 15:13:32 -0500

    Fleshes out template some:
     - package.json
     - webpack config
     - react and less dependencies
     - simple README instructions
     - express server

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [{
+      test: /\.js|\.jsx$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    },
+      {
+        test: /\.less$/,
+        loader: "style!css!less"
+      }]
+  }
+};
